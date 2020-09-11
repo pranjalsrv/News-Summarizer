0f6ecb619a6dc6de0adef919e12c0e7f1e2197fa
pranjalsrv <pranjalsrv@gmail.com>
2020-09-11 11:59:43 +0530

    added archiving and view count

--- ElasticSearch Pipeline/app.py
+++ ElasticSearch Pipeline/app.py
@@ -2,28 +2,38 @@ from fastapi import FastAPI
 from elasticsearch import Elasticsearch
 from fastapi.responses import JSONResponse
 
-
 app = FastAPI()
 es = Elasticsearch()
+headers = {'content-type': "application/json"}
+
 
 @app.get("/get_category_news/")
-async def category(category:str) -> JSONResponse:
-    headers = {'content-type': "application/json"}
-    res = es.search(index = "news", body = {"from":0, "size":10, 
-    "query":{"match": {"category": category}}, "sort": {
-        "pubDate": {
-        "order": "desc"
-        }
-    }})["hits"]["hits"]
-    return JSONResponse(content = [i["_source"] for i in res], headers = headers)
+async def category(category: str) -> JSONResponse:
+    res = es.search(index="news", body={"from": 0, "size": 10,
+                                        "query": {"match": {"category": category}}, "sort": {
+            "pubDate": {
+                "order": "desc"
+            }
+        }})["hits"]["hits"]
+    return JSONResponse(content=[i["_source"] for i in res], headers=headers)
+
 
 @app.get("/get_news/")
-async def query(query:str) -> JSONResponse:
-    headers = {'content-type': "application/json"}
-    res = es.search(index = "news", body = {"from":0, "size":10, 
-    "query":{"match": {"title": query}}, "sort": {
-        "pubDate": {
-        "order": "desc"
-        }
-    }})["hits"]["hits"]
-    return JSONResponse(content = [i["_source"] for i in res], headers = headers)
+async def query(query: str) -> JSONResponse:
+    res = es.search(index="news", body={"from": 0, "size": 10,
+                                        "query": {"match": {"title": query}}, "sort": {
+            "pubDate": {
+                "order": "desc"
+            }
+        }})["hits"]["hits"]
+    return JSONResponse(content=[i["_source"] for i in res], headers=headers)
+
+
+@app.post("/make_view/")
+async def view(article_id: str) -> JSONResponse:
+    article = es.get(index="news", id=article_id)["_source"]
+    article["viewCount"] += 1  # Increase view count
+    if article["pubDate"] < datetime.datetime.now() - datetime.timedelta(days=3) and \
+            article["viewCount"] > 10:  # If older than 3 days and view count>10 then archive
+        article["archived"] = True
+    es.index(index="news", doc_type="news-obj", id=article_id, body=article)  # Performing Update
--- ElasticSearch Pipeline/updater.py
+++ ElasticSearch Pipeline/updater.py
@@ -8,80 +8,79 @@ import elasticsearch
 
 from bs4 import BeautifulSoup
 
-
 maps = {
     "INDIA":
-      "https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
-  "WORLD": "https://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
-  "NRI": "https://timesofindia.indiatimes.com/rssfeeds/7098551.cms",
-  "BUSINESS": "https://timesofindia.indiatimes.com/rssfeeds/1898055.cms",
-  "CRICKET": "https://timesofindia.indiatimes.com/rssfeeds/54829575.cms",
-  "SPORTS": "https://timesofindia.indiatimes.com/rssfeeds/4719148.cms",
-  "SCIENCE":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128672765.cms",
-  "ENVIRONMENT":
-      "http://timesofindia.indiatimes.com/rssfeeds/2647163.cms",
-  "TECH": "http://timesofindia.indiatimes.com/rssfeeds/66949542.cms",
-  "EDUCATION":
-      "http://timesofindia.indiatimes.com/rssfeeds/913168846.cms",
-  "ENTERTAINMENT":
-      "http://timesofindia.indiatimes.com/rssfeeds/1081479906.cms",
-  "LIFESTYLE": "http://timesofindia.indiatimes.com/rssfeeds/2886704.cms",
-  "ASTROLOGY":
-      "https://timesofindia.indiatimes.com/rssfeeds/65857041.cms",
-  "AUTO": "https://timesofindia.indiatimes.com/rssfeeds/74317216.cms",
-  "MUMBAI":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128838597.cms",
-  "DELHI": "http://timesofindia.indiatimes.com/rssfeeds/-2128839596.cms",
-  "BANGALORE":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms",
-  "HYDERABAD":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128816011.cms",
-  "CHENNAI": "http://timesofindia.indiatimes.com/rssfeeds/2950623.cms",
-  "AHEMDABAD":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128821153.cms",
-  "ALLAHBAD": "http://timesofindia.indiatimes.com/rssfeeds/3947060.cms",
-  "BHUBANESHWAR":
-      "http://timesofindia.indiatimes.com/rssfeeds/4118235.cms",
-  "COIMBATORE":
-      "http://timesofindia.indiatimes.com/rssfeeds/7503091.cms",
-  "GURGAON": "http://timesofindia.indiatimes.com/rssfeeds/6547154.cms",
-  "GUWAHATI": "http://timesofindia.indiatimes.com/rssfeeds/4118215.cms",
-  "HUBLI": "http://timesofindia.indiatimes.com/rssfeeds/3942695.cms",
-  "KANPUR": "http://timesofindia.indiatimes.com/rssfeeds/3947067.cms",
-  "KOLKATA":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128830821.cms",
-  "LUDHIANA": "http://timesofindia.indiatimes.com/rssfeeds/3947051.cms",
-  "MANGALORE": "http://timesofindia.indiatimes.com/rssfeeds/3942690.cms",
-  "MYSORE": "http://timesofindia.indiatimes.com/rssfeeds/3942693.cms",
-  "NOIDA": "http://timesofindia.indiatimes.com/rssfeeds/8021716.cms",
-  "PUNE": "http://timesofindia.indiatimes.com/rssfeeds/-2128821991.cms",
-  "GOA": "http://timesofindia.indiatimes.com/rssfeeds/3012535.cms",
-  "CHANDIGARH":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128816762.cms",
-  "LUCKNOW":
-      "http://timesofindia.indiatimes.com/rssfeeds/-2128819658.cms",
-  "PATNA": "http://timesofindia.indiatimes.com/rssfeeds/-2128817995.cms",
-  "JAIPUR": "http://timesofindia.indiatimes.com/rssfeeds/3012544.cms",
-  "NAGPUR": "http://timesofindia.indiatimes.com/rssfeeds/442002.cms",
-  "RAJKOT": "http://timesofindia.indiatimes.com/rssfeeds/3942663.cms",
-  "RANCHI": "http://timesofindia.indiatimes.com/rssfeeds/4118245.cms",
-  "SURAT": "http://timesofindia.indiatimes.com/rssfeeds/3942660.cms",
-  "VADODARA": "http://timesofindia.indiatimes.com/rssfeeds/3942666.cms",
-  "VARANASI": "http://timesofindia.indiatimes.com/rssfeeds/3947071.cms",
-  "THANE": "http://timesofindia.indiatimes.com/rssfeeds/3831863.cms",
-  "THIRUVANANTHAPURAM":
-      "http://timesofindia.indiatimes.com/rssfeeds/878156304.cms",
-  "USA": "http://timesofindia.indiatimes.com/rssfeeds/30359486.cms",
-  "SOUTH_ASIA":
-      "http://timesofindia.indiatimes.com/rssfeeds/3907412.cms",
-  "UK": "http://timesofindia.indiatimes.com/rssfeeds/2177298.cms",
-  "EUROPE": "http://timesofindia.indiatimes.com/rssfeeds/1898274.cms",
-  "CHINA": "http://timesofindia.indiatimes.com/rssfeeds/1898184.cms",
-  "MIDDLE_EAST":
-      "http://timesofindia.indiatimes.com/rssfeeds/1898272.cms",
-  "REST_OF_WORLD":
-      "http://timesofindia.indiatimes.com/rssfeeds/671314.cms"
+        "https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
+    "WORLD": "https://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
+    "NRI": "https://timesofindia.indiatimes.com/rssfeeds/7098551.cms",
+    "BUSINESS": "https://timesofindia.indiatimes.com/rssfeeds/1898055.cms",
+    "CRICKET": "https://timesofindia.indiatimes.com/rssfeeds/54829575.cms",
+    "SPORTS": "https://timesofindia.indiatimes.com/rssfeeds/4719148.cms",
+    "SCIENCE":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128672765.cms",
+    "ENVIRONMENT":
+        "http://timesofindia.indiatimes.com/rssfeeds/2647163.cms",
+    "TECH": "http://timesofindia.indiatimes.com/rssfeeds/66949542.cms",
+    "EDUCATION":
+        "http://timesofindia.indiatimes.com/rssfeeds/913168846.cms",
+    "ENTERTAINMENT":
+        "http://timesofindia.indiatimes.com/rssfeeds/1081479906.cms",
+    "LIFESTYLE": "http://timesofindia.indiatimes.com/rssfeeds/2886704.cms",
+    "ASTROLOGY":
+        "https://timesofindia.indiatimes.com/rssfeeds/65857041.cms",
+    "AUTO": "https://timesofindia.indiatimes.com/rssfeeds/74317216.cms",
+    "MUMBAI":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128838597.cms",
+    "DELHI": "http://timesofindia.indiatimes.com/rssfeeds/-2128839596.cms",
+    "BANGALORE":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128833038.cms",
+    "HYDERABAD":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128816011.cms",
+    "CHENNAI": "http://timesofindia.indiatimes.com/rssfeeds/2950623.cms",
+    "AHEMDABAD":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128821153.cms",
+    "ALLAHBAD": "http://timesofindia.indiatimes.com/rssfeeds/3947060.cms",
+    "BHUBANESHWAR":
+        "http://timesofindia.indiatimes.com/rssfeeds/4118235.cms",
+    "COIMBATORE":
+        "http://timesofindia.indiatimes.com/rssfeeds/7503091.cms",
+    "GURGAON": "http://timesofindia.indiatimes.com/rssfeeds/6547154.cms",
+    "GUWAHATI": "http://timesofindia.indiatimes.com/rssfeeds/4118215.cms",
+    "HUBLI": "http://timesofindia.indiatimes.com/rssfeeds/3942695.cms",
+    "KANPUR": "http://timesofindia.indiatimes.com/rssfeeds/3947067.cms",
+    "KOLKATA":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128830821.cms",
+    "LUDHIANA": "http://timesofindia.indiatimes.com/rssfeeds/3947051.cms",
+    "MANGALORE": "http://timesofindia.indiatimes.com/rssfeeds/3942690.cms",
+    "MYSORE": "http://timesofindia.indiatimes.com/rssfeeds/3942693.cms",
+    "NOIDA": "http://timesofindia.indiatimes.com/rssfeeds/8021716.cms",
+    "PUNE": "http://timesofindia.indiatimes.com/rssfeeds/-2128821991.cms",
+    "GOA": "http://timesofindia.indiatimes.com/rssfeeds/3012535.cms",
+    "CHANDIGARH":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128816762.cms",
+    "LUCKNOW":
+        "http://timesofindia.indiatimes.com/rssfeeds/-2128819658.cms",
+    "PATNA": "http://timesofindia.indiatimes.com/rssfeeds/-2128817995.cms",
+    "JAIPUR": "http://timesofindia.indiatimes.com/rssfeeds/3012544.cms",
+    "NAGPUR": "http://timesofindia.indiatimes.com/rssfeeds/442002.cms",
+    "RAJKOT": "http://timesofindia.indiatimes.com/rssfeeds/3942663.cms",
+    "RANCHI": "http://timesofindia.indiatimes.com/rssfeeds/4118245.cms",
+    "SURAT": "http://timesofindia.indiatimes.com/rssfeeds/3942660.cms",
+    "VADODARA": "http://timesofindia.indiatimes.com/rssfeeds/3942666.cms",
+    "VARANASI": "http://timesofindia.indiatimes.com/rssfeeds/3947071.cms",
+    "THANE": "http://timesofindia.indiatimes.com/rssfeeds/3831863.cms",
+    "THIRUVANANTHAPURAM":
+        "http://timesofindia.indiatimes.com/rssfeeds/878156304.cms",
+    "USA": "http://timesofindia.indiatimes.com/rssfeeds/30359486.cms",
+    "SOUTH_ASIA":
+        "http://timesofindia.indiatimes.com/rssfeeds/3907412.cms",
+    "UK": "http://timesofindia.indiatimes.com/rssfeeds/2177298.cms",
+    "EUROPE": "http://timesofindia.indiatimes.com/rssfeeds/1898274.cms",
+    "CHINA": "http://timesofindia.indiatimes.com/rssfeeds/1898184.cms",
+    "MIDDLE_EAST":
+        "http://timesofindia.indiatimes.com/rssfeeds/1898272.cms",
+    "REST_OF_WORLD":
+        "http://timesofindia.indiatimes.com/rssfeeds/671314.cms"
 }
 
 es = elasticsearch.Elasticsearch()
@@ -91,25 +90,27 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = transformers.T5ForConditionalGeneration.from_pretrained('t5-base').to(device)
 tokenizer = transformers.T5Tokenizer.from_pretrained('t5-base')
 
+
 def preprocess_text(url, tag):
     response = requests.get(url)
     content = response.content
     soup_article = BeautifulSoup(content, "html5lib")
-    body = soup_article.find_all(tag, class_ = None)
+    body = soup_article.find_all(tag, class_=None)
     s = ""
     for i in body:
-        s+=i.text
+        s += i.text
     return s
+
 
 def summarize(text):
     t5_prep = "summarize: " + str(text).strip().replace("\n", "")
-    tokenized_text = tokenizer.encode(t5_prep, max_length = len(t5_prep), return_tensors = "pt", truncation = True).to(device)
-    summary_ids = model.generate(tokenized_text, num_beams = 4,
-                                 no_repeat_ngram_size = 2,
-                                 min_length = 120,
-                                 max_length = 350,
-                                 early_stopping = True)
-    output = tokenizer.decode(summary_ids[0].to(device), skip_special_tokens = True)
+    tokenized_text = tokenizer.encode(t5_prep, max_length=len(t5_prep), return_tensors="pt", truncation=True).to(device)
+    summary_ids = model.generate(tokenized_text, num_beams=4,
+                                 no_repeat_ngram_size=2,
+                                 min_length=120,
+                                 max_length=350,
+                                 early_stopping=True)
+    output = tokenizer.decode(summary_ids[0].to(device), skip_special_tokens=True)
     return output
 
 
@@ -117,7 +118,7 @@ if __name__ == "__main__":
     for key, value in maps.items():
         maps[key] = {"link": value, "lastPub": datetime.datetime(2018, 9, 10, 6, 36, 43)}
 
-    while(True):
+    while True:
         for key, value in maps.items():
             app = dict(xmltodict.parse(requests.get(value["link"]).content.decode("utf-8"))["rss"].pop("channel"))
             time = value["lastPub"]
@@ -125,22 +126,22 @@ if __name__ == "__main__":
                 try:
                     item = dict(i)
                     item["pubDate"] = datetime.datetime.strptime(" ".join(item["pubDate"].split()[1:-1]), "%d %b %Y %X")
-                    if(item["pubDate"] > value["lastPub"]):
+                    if item["pubDate"] > value["lastPub"]:
                         item["category"] = key
                         item["archived"] = False
                         item.pop("guid")
 
                         item["summary"] = summarize(preprocess_text(item["link"], "div")) + "......."
+                        item["viewCount"] = 0
 
-                        es.index(index = "news", doc_type = "news-obj", id = uuid.uuid1(), body = item)
-                        if(item["pubDate"] > time):
+                        es.index(index="news", doc_type="news-obj", id=uuid.uuid1(), body=item)
+                        if item["pubDate"] > time:
                             time = item["pubDate"]
 
                     del item
                 except:
                     continue
 
-            
             del app
             maps[key]["lastPub"] = time
             del time
